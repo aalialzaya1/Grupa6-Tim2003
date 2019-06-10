58ecd54875043714d8f60938e47d6b4aef5e9728
ldrkic1 <ldrkic1@etf.unsa.ba>
2019-06-10 14:04:27 +0200

    DatabaseContext

--- LetUsBringYourHomeStyle/Models/DatabaseContext.cs
+++ LetUsBringYourHomeStyle/Models/DatabaseContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static LetUsBringYourHomeStyle.Models.homeStyle;
 
 namespace LetUsBringYourHomeStyle.Models
 {
@@ -17,6 +18,18 @@ namespace LetUsBringYourHomeStyle.Models
         {
             instance = this;
         }
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Firma> Firma { get; set; }
+        public DbSet<Namjestaj> Namjestaj { get; set; }
+        public DbSet<PredstavnikFirme> PredstavnikFirme { get; set; }
+        public DbSet<Popust> Popust { get; set; }
+        public DbSet<Recenzija> Recenzija { get; set; }
+        //public DbSet<TEntity> Kategorija { get; set; }
+        public DbSet<RegistrovaniKorisnik> RegistrovaniKorisnik { get; set; }
+        public DbSet<SistemskiKorisnik> SistemskiKorisnik { get; set; } // sistemski
+        public DbSet<Savjet> Savjet { get; set; }//savjet
+
+        public DbSet<Dimenzija> Dimenzija { get; set; }
 
     }
 }
