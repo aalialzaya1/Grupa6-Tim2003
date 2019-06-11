6e7d3e6654b4a84c4be015948ed52c11c7810825
ldrkic1 <ldrkic1@etf.unsa.ba>
2019-06-11 16:46:03 +0200

    prepravke

--- LetUsBringYourHomeStyle/Models/DatabaseContext.cs
+++ LetUsBringYourHomeStyle/Models/DatabaseContext.cs
@@ -21,12 +21,10 @@ namespace LetUsBringYourHomeStyle.Models
         public DbSet<Admin> Admin { get; set; }
         public DbSet<Firma> Firma { get; set; }
         public DbSet<Namjestaj> Namjestaj { get; set; }
-        public DbSet<PredstavnikFirme> PredstavnikFirme { get; set; }
         public DbSet<Popust> Popust { get; set; }
         public DbSet<Recenzija> Recenzija { get; set; }
         //public DbSet<TEntity> Kategorija { get; set; }
         public DbSet<RegistrovaniKorisnik> RegistrovaniKorisnik { get; set; }
-        public DbSet<SistemskiKorisnik> SistemskiKorisnik { get; set; }
         public DbSet<Savjet> Savjet { get; set; }
         public DbSet<Dimenzija> Dimenzija { get; set; }
 
@@ -35,10 +33,8 @@ namespace LetUsBringYourHomeStyle.Models
             modelBuilder.Entity<Admin>().ToTable("Admin");
             modelBuilder.Entity<Firma>().ToTable("Firma");
             modelBuilder.Entity<Namjestaj>().ToTable("Namjestaj");
-            modelBuilder.Entity<PredstavnikFirme>().ToTable("PredstavnikFirme");
             modelBuilder.Entity<Popust>().ToTable("Recenzija");
             modelBuilder.Entity<RegistrovaniKorisnik>().ToTable("RegistrovaniKorisnik");
-            modelBuilder.Entity<SistemskiKorisnik>().ToTable("SistemskiKorisnik");
             modelBuilder.Entity<Savjet>().ToTable("Savjet");
             modelBuilder.Entity<Dimenzija>().ToTable("Dimenzija");
         }
